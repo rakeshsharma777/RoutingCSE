f48b950fe0b210c956c25e754485c8e00132d332
rakeshsharma <rakeshsharma@gmail.com>
2026-02-11 12:32:38 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import Home from "./Components/Home"
-import Dashboard from "./Components/Dashboard"
-import Login from "./Components/Login"
-import { BrowserRouter,Routes,Route } from "react-router-dom"
+
 function App() {
  
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <h1>Welcome to my website</h1>
+  )
 
 
 
